3b242c9c662c6e8c4ac773d1bc384c4e5dacdf25
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-07-26 23:44:58 +0530

    Add files via upload

---
+++ mathematicalOperation.sh
@@ -0,0 +1,18 @@
+# This file contains all basic mathematical operation 
+
+# Addition
+a=10
+b=19
+add=$(($a+$b))
+echo "sum is : $add"
+
+sub=$(($a-$b))
+echo "subtraction is : $sub"
+
+mul=$(($a*$b))
+echo "multiplication is : $mul"
+
+div=$(($a/$b))
+echo "division is : $div"
+
+
