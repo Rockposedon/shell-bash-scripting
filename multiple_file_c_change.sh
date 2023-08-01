b4a2cd72d92bbe86d06fb7e10daacf834f749a7b
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-01 23:53:26 +0530

    Add files via upload

---
+++ multiple_file_c_change.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+read -p "enter file type : " filetype
+read -p " search text : " search
+read -p " replace text :" replace
+
+if [ $filetype != "" ]
+then
+	sed -i "s/$search/$replace/" *.txt
+fi
+
