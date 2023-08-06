a51e43081ac9694088180bdd8241642187991167
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-06 22:23:20 +0530

    Create New_user(task-5).sh

---
+++ New_user(task-5).sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# To add New User and -m does the user’s home directory will be created if it does not exist.
+sudo useradd -m pankaj
+# To add password to newly created user 
+sudo passwd pankaj
+
