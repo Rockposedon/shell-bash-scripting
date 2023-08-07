ffc97b38ad5762d8e4925a4b44f1ad7971f9e919
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-07 23:39:19 +0530

    Update UserInput.sh

--- UserInput.sh
+++ UserInput.sh
@@ -1,23 +1,23 @@
-# taking inputs from user and prints it  
-echo "enter name :"
+# taking inputs from the user and printing it  
+echo "enter name:"
 read name1 name2 name3 name4
 echo "names : $name1,$name2,$name3,$name4"
 
-# taking n numbers inputs from user in single line 
+# taking n numbers inputs from the user in a single line 
 read -p "enter school:" schoolname
 echo $schoolname
 
-# sp is uesd to hide the data written by user in shell
-read -sp "password : " password
+# sp is used to hide the data written by the user in the shell
+read -sp "password: " password
 echo $password
 
-# -a is used to define array and we can access array elements using indexing
+# -a is used to define an array and we can access array elements using indexing
 echo "enter array elements"
 read -a array
 echo "elements are :${array[0]},${array[2]}"
 
-# if we don't declare variables in read command then the inbulit default variable call by the system
+# if we don't declare variables in the read command then the inbuilt default variable call by the system
 # and we can access it by system variable "REPLY"
 read 
-echo "elements are : " $REPLY
+echo "elements are: " $REPLY
 
