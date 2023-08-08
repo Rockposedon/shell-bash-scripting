c0878555a46650778accce00d26764266263f555
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:49:34 +0530

    Update file_finder_py_json_txt(task-1).sh

--- file_finder_py_json_txt(task-1).sh
+++ file_finder_py_json_txt(task-1).sh
@@ -1,6 +1,10 @@
 #! /usr/bin/bash
+# In this bash script we are finding all files in a system whose extensions are (python, json, and text)
+read -p "enter the file you want to search: " filename
 
-read -p "enter the file you want to search : " filename
+# 2>/dev/null is a special type of file that every Linux distro has.  It is a black hole in the operating system where anything put into it is discarded.
+# 2>/dev/null is used to remove file not found error from the console
+
 if [ $filename == "python" ]
 then
 	find / -name '*.py' 2>/dev/null
@@ -11,6 +15,6 @@ elif [ $filename == "txt" ]
 then
 	find / -name '*.txt' 2>/dev/null
 else
-	echo "you entered wrong format"
+	echo "entered format not matched"
 fi
 
