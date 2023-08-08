fd427405d142926ef2dd58c72115120fd130b738
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:42:58 +0530

    Update change_content(task-2).sh

--- change_content(task-2).sh
+++ change_content(task-2).sh
@@ -1,9 +1,14 @@
 #! /usr/bin/bash
+# In this bash script, we are replacing a word from the update.txt file 
 filename=update.txt
-read -p "enter text to serach :" search
-read -p "enter text to replace serached text : " replace
+# 2 inputs will be given by the user(search, replace)
+read -p "enter text to search:" search
+read -p "enter text to replace searched text: " replace
+
 if [ $filename == update.txt ];
 then
 	sed -i " s/$search/$replace/ " $filename
+else 
+	echo "$filename is not present"
 fi
 
