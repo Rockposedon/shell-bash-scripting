0b66c8813abbd40b20963ba481e0efad036cb573
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:39:14 +0530

    comments updated in select_loop.sh

--- select_loop.sh
+++ select_loop.sh
@@ -1,6 +1,12 @@
-#! /usr/bin/bash
-# select loop is used to create menus from the list
-select name in p a rr i t o s h
-do 
-	echo "$name selected"
+#!/usr/bin/bash
+
+# This script demonstrates the usage of the 'select' loop to create a menu
+# from a list of options and then displays a message based on the user's selection.
+
+# Start the 'select' loop to present a menu of options
+select name in p a rr i t o s h; do
+    # Display a message indicating the selected option
+    echo "$name selected"
 done
+
+# The 'select' loop is utilized to generate a menu from the provided list of options.
