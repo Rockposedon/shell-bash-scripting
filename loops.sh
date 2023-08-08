61413c10532596ad621eb7a2a91e0a31be5a6ba8
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:00:40 +0530

    Update loops.sh

--- loops.sh
+++ loops.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
-for i in {1..10}; do
-	echo " $i"
+
+# This is a bash script that iterates through items (files and directories)
+# in the current directory and prints the names of directories.
+
+# The following loop iterates through each item in the current directory.
+for item in *
+do
+    # Check if the current item is a directory.
+    if [ -d "$item" ]; then
+        # If the item is a directory, execute the following block of code.
+        
+        # Print the name of the directory to the console.
+        echo "$item"
+    fi
 done
 
-for i in {1..100}; do
-	echo -n $i
-done
-
+# The loop iterates through all items (files and directories) in the current directory.
+# It checks if each item is a directory and if so, it prints the name of the directory.
