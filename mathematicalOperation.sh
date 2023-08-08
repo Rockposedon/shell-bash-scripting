3e0745b9c60d5ffa884a0fa5e72903f9a5f47cb5
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:23:31 +0530

    some features is added to mathematicalOperation.sh

--- mathematicalOperation.sh
+++ mathematicalOperation.sh
@@ -1,18 +1,23 @@
-# This file contains all basic mathematical operation 
+#!/usr/bin/bash
 
-# Addition
+# This script demonstrates basic mathematical operations.
+
+# Assign values to variables for addition
 a=10
 b=19
-add=$(($a+$b))
-echo "sum is : $add"
 
-sub=$(($a-$b))
-echo "subtraction is : $sub"
+# Perform addition and store the result in the 'add' variable
+add=$(($a + $b))
+echo "Sum is : $add"
 
-mul=$(($a*$b))
-echo "multiplication is : $mul"
+# Perform subtraction and store the result in the 'sub' variable
+sub=$(($a - $b))
+echo "Subtraction is : $sub"
 
-div=$(($a/$b))
-echo "division is : $div"
+# Perform multiplication and store the result in the 'mul' variable
+mul=$(($a * $b))
+echo "Multiplication is : $mul"
 
-
+# Perform division and store the result in the 'div' variable
+div=$(($a / $b))
+echo "Division is : $div"
