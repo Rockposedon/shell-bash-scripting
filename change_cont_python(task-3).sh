74d3c32ec18601a48efa2124673d6118629012fe
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:05:36 +0530

    code being updated

--- change_cont_python(task-3).sh
+++ change_cont_python(task-3).sh
@@ -1,23 +1,12 @@
-#!/usr/bin/bash
+#! /usr/bin/bash
+# In this bash script, we are replacing a word from the file 
 
-# This script demonstrates basic mathematical operations.
+# All the 3 inputs will be given by the user(filename, search, replaced)
+read -p "enter file name: "  filename
+read -p "enter text to search: " search
+read -p "enter text to replace searched text: " replace
 
-# Assign values to variables for addition
-a=10
-b=19
-
-# Perform addition and store the result in the 'add' variable
-add=$(($a + $b))
-echo "Sum is : $add"
-
-# Perform subtraction and store the result in the 'sub' variable
-sub=$(($a - $b))
-echo "Subtraction is : $sub"
-
-# Perform multiplication and store the result in the 'mul' variable
-mul=$(($a * $b))
-echo "Multiplication is : $mul"
-
-# Perform division and store the result in the 'div' variable
-div=$(($a / $b))
-echo "Division is : $div"
+# Here we are replacing Word from the python files 
+if [ $filename == '*.py' ];
+then
+        sed -i " s/$search/$replace/ " $filename
