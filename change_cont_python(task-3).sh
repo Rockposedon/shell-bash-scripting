63c01b12102f8272d760470a7cbf38f0fb4a4ab2
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:37:24 +0530

    Code being updated

--- change_cont_python(task-3).sh
+++ change_cont_python(task-3).sh
@@ -1,7 +1,11 @@
-#!/usr/bin/bash
-read -p "enter file name : "  filename
-read -p "enter text to serach : " search
-read -p "enter text to replace serached text : " replace
+#! /usr/bin/bash
+# In this bash script, we are replacing a word from the file 
+# All the 3 inputs will be given by the user(filename, search, replaced)
+read -p "enter file name: "  filename
+read -p "enter text to search: " search
+read -p "enter text to replace searched text: " replace
+
+# Here we are replacing Word from python files 
 if [ $filename == '*.py' ];
 then
         sed -i " s/$search/$replace/ " $filename
