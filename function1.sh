98699eeaf9b261d54533ec422024f43e87fd235e
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:56:29 +0530

    Update function1.sh

--- function1.sh
+++ function1.sh
@@ -1,15 +1,27 @@
+#!/bin/bash
+
+# Define function func1
 func1()
 {
+    # Declare a local variable 'var' within func1
     local var='func1 local'
+    
+    # Call function func2 from within func1
     func2
 }
 
+# Define function func2
 func2()
 {
+    # Display the value of 'var', but this won't work as expected
     echo "In func2, var = $var"
 }
 
+# Set a global variable 'var'
 var=global
+
+# Call func1, which in turn calls func2
 func1
 
+# Demonstrate brace expansion in echo command
 echo a{p,a,r,o}e
