ccf0abe8abe5e8b0171f4593228a8e088da9cd76
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:38:09 +0530

    comments added to select_item_from_file.sh

--- select_item_from_file.sh
+++ select_item_from_file.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/bash
-select item in name.txt
-do 
-	echo "$item selected"
+# This script uses a 'select' loop to present a menu with a single item, 
+# which is the filename "name.txt", and then displays a message 
+# based on the user's selection.
+
+# Start the 'select' loop
+select item in name.txt; do
+    # Display a message indicating the selected item
+    echo "$item selected"
 done
 
+# The 'select' loop here is used to create a simple menu with only one option.
