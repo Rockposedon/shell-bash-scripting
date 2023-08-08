7709d226d53dc2594e4cff185c81a0af8bc3ab48
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:55:13 +0530

    code being updated

--- folder_creation.sh
+++ folder_creation.sh
@@ -1,10 +1,16 @@
-#! /usr/bin/bash
-read -p "enter folder name :" folderName
-read -p "enter folder number :" folderNumber
+#!/usr/bin/bash
 
+# Prompt the user for the folder name
+read -p "Enter folder name: " folderName
+
+# Prompt the user for the folder number
+read -p "Enter folder number: " folderNumber
+
+# Create the main folder with the provided name
 mkdir -p "$folderName"
+
+# Loop to create additional folders with incremental numbers
 for((i=2; i<=$folderNumber; i++)); do
+  # Create folders with names like "folderName_2", "folderName_3", ...
   mkdir -p "$folderName"_$i
 done
-
-
