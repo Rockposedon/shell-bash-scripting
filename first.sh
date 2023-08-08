bbc9da5318a93123a7d6f1879ddac5af7481ff1d
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:53:32 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,43 +1,43 @@
-#! /bin/bash
+#!/bin/bash
+
+# Display a greeting message
 echo "hello world"
-echo "I am using vim editor to add content of this file "
+echo "I am using vim editor to add content to this file"
 
-#variables:
+# Variables:
 
-# System variables 
-echo $HOME
-echo $BASH
-echo $BASH_VERSION
-echo $PWD
+# System variables
+echo $HOME          # Display the user's home directory
+echo $BASH          # Display the path of the current Bash shell
+echo $BASH_VERSION  # Display the version of Bash
+echo $PWD           # Display the present working directory
 
-# User defined varibale
-
-name=Paritosh  
+# User-defined variables
+name=Paritosh
 class=MCA-7th-sem
 college=IIPS
 
-#Using above variables with the help of echo command
+# Using the user-defined variables with the echo command
+echo "I am $name, a student of $college studying in $class"
 
-echo "I am $name student of $college studying in $class"
-
-# addition
+# Addition
 a=10
 b=20
-# 1st approch to add two variables 
-c=$(($a+$b))
+
+# 1st approach to add two variables
+c=$(($a + $b))
 echo $c
-2nd approch to add two variables
-sum=`expr $a +$b`
+
+# 2nd approach to add two variables
+sum=`expr $a + $b`
 echo $sum
-3rd approch to add two variables
-sum1= $(expr $a + $b)
+
+# 3rd approach to add two variables
+sum1=$(expr $a + $b)
 echo $sum1
 
-# User input 
-read -p "enter num1 : " num1
-read -p "enter num2 : " num2
-fin=$(($num1+$num2))
-echo "sum is : $fin"
-
-
-
+# User input
+read -p "Enter num1: " num1
+read -p "Enter num2: " num2
+fin=$(($num1 + $num2))
+echo "Sum is: $fin"
