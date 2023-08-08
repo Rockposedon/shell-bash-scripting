c77980312b55353c72fd64a2b0ceea3916c3f990
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:25:33 +0530

    added some comments multiple_file_content_change(task-4).sh

--- multiple_file_content_change(task-4).sh
+++ multiple_file_content_change(task-4).sh
@@ -1,11 +1,16 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 
-read -p "enter file type: " filetype
-read -p " search text: " search
-read -p " replace text:" replace
+# Prompt the user to input the file type, search text, and replace text
+read -p "Enter file type: " filetype
+read -p "Enter search text: " search
+read -p "Enter replace text: " replace
 
-if [ $filetype != "" ]
-then
-	sed -i "s/$search/$replace/" *.txt
+# Check if the entered file type is not empty
+if [ "$filetype" != "" ]; then
+    # Use sed to search and replace text in all .txt files in the current directory
+    sed -i "s/$search/$replace/g" *."$filetype"
+    echo "Text replacement completed."
+else
+    # Display an error message if the file type is empty
+    echo "File type cannot be empty."
 fi
-
