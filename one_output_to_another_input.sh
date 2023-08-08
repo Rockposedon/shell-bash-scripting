4e8dd9b788b10fe21866676b979c864b08ddaf32
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 09:35:33 +0530

    code added to one_output_to_another_input.sh

--- one_output_to_another_input.sh
+++ one_output_to_another_input.sh
@@ -1,14 +1,22 @@
 #! /usr/bin/bash
-read -p "enter file name :" file_name
-if [ -f $file_name ]
-then 
-	if [ -w $file_name ]
-	then
-		echo "type something or press ctrl+d to quit"
-	cat >> $file_name
-	else
-		echo "The file do not have write permission"
-	fi
+
+# Prompt the user to enter a file name
+read -p "Enter file name: " file_name
+
+# Check if the specified file exists
+if [ -f "$file_name" ]; then
+    # Check if the file has write permission
+    if [ -w "$file_name" ]; then
+        # Inform the user to input text or press Ctrl+D to quit
+        echo "Type something or press Ctrl+D to quit"
+        
+        # Allow the user to append text to the specified file using the 'cat' command
+        cat >> "$file_name"
+    else
+        # Display a message if the file does not have write permission
+        echo "The file does not have write permission."
+    fi
 else
-	echo "$file_name not exists"
+    # Display a message if the specified file does not exist
+    echo "$file_name does not exist."
 fi
