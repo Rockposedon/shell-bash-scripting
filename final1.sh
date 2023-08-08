4dda3eb1514096304d630008f43995d2e33f84c1
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:52:28 +0530

    Added some comments for user readability

--- final1.sh
+++ final1.sh
@@ -1,16 +1,23 @@
-#! bin/bash
+#!/bin/bash
 
-read -p "enter number of file you want to create : " no_file
-read -p "enter path :" path
-a=0
-for i in $(seq ${a} ${no_file}) 
-do
-if [ -d $path ]
-then
-    mkdir $(date +%y-%m-%d -d "$date + $i day") 
-else 
-    echo "dir not exits"
+# Prompt the user for the number of directories to create.
+read -p "Enter the number of directories you want to create: " no_dir
+
+# Prompt the user for the path where the directories should be created.
+read -p "Enter the path where you want to create directories: " path
+
+# Check if the specified path is a directory.
+if [ -d "$path" ]; then
+    # Loop to create the specified number of directories.
+    for i in $(seq 0 $((no_dir - 1)))
+    do
+        # Calculate the date based on the loop iteration.
+        dir_name=$(date +%y-%m-%d -d "$i day")
+        
+        # Create the directory with the calculated name under the specified path.
+        mkdir -p "$path/$dir_name"
+    done
+else
+    # If the specified path is not a directory, show an error message.
+    echo "Directory does not exist."
 fi
-done
-
-
