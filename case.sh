f1d9ae48207897d3bc5a48b3d9226670b66863b1
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-08-08 08:29:45 +0530

    Added more code

--- case.sh
+++ case.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/bash
+#! /usr/bin/bash
+# $1 represents the first argument passed to the function or a bash script
+# In this bash script, how the case statement works, is explained 
+# If the user gives a car as an input it will print 1st line and so on  
 vehicle=$1
-case $vehicle in 
+case $vehicle in  
 	"car")
 		echo "the rent of $vehicle is 120 rupees";;
 	"cycle")
@@ -8,5 +11,6 @@ case $vehicle in
 	"bike")
 		echo "the rent of $vehicle is 50 rupees";;
 	*)
-		echo "undifined type";;
+		echo "undefined type";
+# end of the case 
 esac
